8925c55be1b407e7e9a10c0897ad153777b1d7ea
naban <padmanabhan1997@gmail.com>
2025-11-09 11:04:22 +0530

    basic changes

--- src/main/java/com/pn/librarybookmanagement/LibraryBookManagementApplication.java
+++ src/main/java/com/pn/librarybookmanagement/LibraryBookManagementApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryBookManagementApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryBookManagementApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryBookManagementApplication.class, args);
+    }
 
 }
